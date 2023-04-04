d4c02b6fb59878954a616a9e0fb257ed76bc78fb
icp049 <108185265+icp049@users.noreply.github.com>
2023-04-04 00:46:04 -0600

    Update initial_code.ino

--- initial_code.ino
+++ initial_code.ino
@@ -1,146 +1,79 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
 #include <Servo.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
 
+#define TFT_CS   10
+#define TFT_RST  8
+#define TFT_DC   9
 
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+Servo myservo;
 
+void setup() {
+  tft.initR();
+  tft.fillScreen(ST7735_BLACK);
+  myservo.attach(7);
+  tft.setRotation(1);
 
-const int TRIGGER_PIN = 6;
-const int ECHO_PIN = 5;
-const int NUM_READINGS = 5;
-const int MAX_DISTANCE = 30; 
-const int MIN_DISTANCE = 4; 
-const int BUZZER = 3;
-const int potPin = A0;  
-int potVal; 
-int pos = 0;    
+  tft.drawCircle(80, 120, 30, ST7735_RED);
+  tft.drawCircle(80, 120, 50, ST7735_RED);
 
-Servo myservo; 
-LiquidCrystal_I2C lcd(0x27, 16, 2); // Set the LCD address to 0x27 for a 16x2 display
-
-
-
-
-
-void setup()
-{
-  myservo.attach(7); 
-  Serial.begin(9600);
-  pinMode(TRIGGER_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
-  pinMode(BUZZER, OUTPUT);
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(0,0);
+  tft.setCursor(0, 0);
+  tft.setTextColor(ST7735_YELLOW);
+  tft.setTextSize(1);
+  tft.print("Servo position:");
+  
 }
 
-void loop()
-{
-  float totalDistance = 0;
+void loop() {
+ 
 
-   for (int i = 0; i < NUM_READINGS; i++) {
-    digitalWrite(TRIGGER_PIN, LOW);
-    delayMicroseconds(2);
-    digitalWrite(TRIGGER_PIN, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIGGER_PIN, LOW);
-  
-    float duration = pulseIn(ECHO_PIN, HIGH);
-    float distance = duration / 58.2;
-  
-    Serial.print("Distance ");
-    Serial.print(i+1);
-    Serial.print(": ");
-    Serial.print(distance);
-    Serial.println(" cm");
-  
-    totalDistance += distance;
-  }
-  
-  float averageDistance = totalDistance / NUM_READINGS;
+  float prev_x, prev_y; // Add variables to store previous endpoint
 
-  Serial.print("Average Distance: ");
-  Serial.print(averageDistance);
-  Serial.println(" cm");
+  // Move servo from 0 to 180
+  for(int pos = 0; pos <= 180; pos++) {
+    tft.drawCircle(80, 120, 30, ST7735_RED);
+    tft.drawCircle(80, 120, 50, ST7735_RED);
+    myservo.write(pos);
+    delay(5);
 
-   if (averageDistance > MAX_DISTANCE) {
-   
-    lcd.setCursor(0, 1);
-    lcd.print("Out of range");
-    lcd.setCursor(12, 1);
-    lcd.print("             ");
-    lcd.setCursor(0, 0);
-    lcd.print("                  ");
-    noTone(BUZZER);
-    delay(100);
+    float x = map(pos, 0, 180, 30, 130);
+    float y = -sqrt(pow(50, 2) - pow(x - 80, 2)) + 120;
     
-    
-    
-  } else if (averageDistance < MIN_DISTANCE) {
-  
-    lcd.setCursor(15, 0);
-    lcd.print(" ");
-    lcd.setCursor(0, 1);
-    lcd.print("IMPACT! IMPACT!"); 
-    lcd.setCursor(0, 0);
-    lcd.print("IMPACT! IMPACT!");
-    tone(BUZZER,500);
-    delay(100);
-  
-    
-   
-    
-    
-  } else {
-    lcd.setCursor(0, 1);
-    lcd.print(averageDistance, 2);
-    lcd.print(" cm");
-    lcd.setCursor(7, 1);
-    lcd.print("            ");
-    lcd.setCursor(0, 0);
-    lcd.print("OBJECT DETECTED!");
-    noTone(BUZZER);
-    delay(100);
-   
-    
-    
-    
-    
-  }
-  
-  delay(200); // Delay for 500 milliseconds (0.5 seconds)
+    tft.fillRect(0, 20, 120, 10, ST7735_BLACK);
+    tft.setCursor(0, 20);
+    tft.setTextColor(ST7735_CYAN);
+    tft.setTextSize(2);
+    tft.print(pos);
 
-  potVal = analogRead(potPin); 
-  
-
-  
-  if (potVal > 0) {
-    
-    int angle = map(potVal, 0, 1023, 0, 180); 
-    myservo.write(angle);  
-    delay(10);            
-  }
-  
-  if(TRIGGER_PIN, HIGH){
-
-      myservo.detach();
-  
-  }else {
-
-    myservo.attach(7); 
-    int angle = 90;
-    
-    for (pos = 90 - angle; pos <= 90 + angle; pos++)
-    {
-      myservo.write(pos);  
-      delay(10);           
-    }
-
-    for (pos = 90 + angle; pos >= 90 - angle; pos--)
-    {
-      myservo.write(pos);  
-      delay(10);          
-    }
+    tft.drawLine(80, 120, prev_x, prev_y, ST7735_BLACK); // Draw over previous line
+    tft.drawLine(80, 120, x, y, ST7735_PWCTR6); // Draw current line
+    prev_x = x; // Store current endpoint as previous endpoint for next iteration
+    prev_y = y;
+    delay(10);
   }
 
+  // Move servo from 180 to 0
+  for(int pos = 180; pos >= 0; pos--) {
+    tft.drawCircle(80, 120, 30, ST7735_RED);
+  tft.drawCircle(80, 120, 50, ST7735_RED);
+
+    myservo.write(pos);
+    delay(5);
+
+    float x = map(pos, 0, 180, 30, 130);
+    float y = -sqrt(pow(50, 2) - pow(x - 80, 2)) + 120;
+    
+    tft.fillRect(0, 20, 120, 10, ST7735_BLACK);
+    tft.setCursor(0, 20);
+    tft.setTextColor(ST7735_CYAN);
+    tft.setTextSize(2);
+    tft.print(pos);
+
+    tft.drawLine(80, 120, prev_x, prev_y, ST7735_BLACK); // Draw over previous line
+    tft.drawLine(80, 120, x, y, ST7735_PWCTR6); // Draw current line
+    prev_x = x; // Store current endpoint as previous endpoint for next iteration
+    prev_y = y;
+    delay(10);
+  }
 }
